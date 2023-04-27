3d7af8cab6fd0c652dff3256e547e5c1aebec840
Abdur Rahman <abdurrahman27485@gmail.com>
2023-04-27 13:57:45 +0600

    hero sectionc done

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div>
-            <h2>This is Hero Section</h2>
+        <div className='hero'>
+            <div className='bg-hero text-white text-center'>
+                <div className='position-absolute top-50 start-50 translate-middle'>
+                    <h1 className='fw-bold'>Hotel Hard Rock</h1>
+                    <h2 className='fw-bold'>A Global Icon Of Laxmipur Luxury</h2>
+                </div>
+            </div>
         </div>
     );
 };
