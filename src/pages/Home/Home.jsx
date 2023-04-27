0eefc092ab9fc3860f06035c9e2b019688e486dd
Abdur Rahman <abdurrahman27485@gmail.com>
2023-04-27 13:11:02 +0600

    site layout created

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from '../../Components/Hero/Hero';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero></Hero>
+            <h2>This Is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
